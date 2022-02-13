8cb64c09756f4c318bfcd27ad7f3b809b7c21825
Felix Mak <82351906+felixmak1107@users.noreply.github.com>
2022-02-13 23:27:03 +0800

    js to ts

--- frontend-ts/src/globalFun.ts
+++ frontend-ts/src/globalFun.ts
@@ -1,8 +1,8 @@
-import Vue from "vue"
+import Vue from 'vue'
 
 Vue.mixin({
 	methods: {
-		hasAuth(perm) {
+		hasAuth(perm: any) {
 			var authority = this.$store.state.permList
 
 			return authority.indexOf(perm) > -1
