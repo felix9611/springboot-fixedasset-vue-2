e831d9d04351148732ab40ecd668cec18fdd171f
Felix Mak <jp.coreching@gmail.com>
2022-04-13 08:53:34 +0800

    fix

--- frontend-ts-class/src/views/asset/assetList/detail.vue
+++ frontend-ts-class/src/views/asset/assetList/detail.vue
@@ -2,7 +2,7 @@
   <div div class="container">
     <div class="handle-box">
         <el-button icon="el-icon-back" circle @click="back"></el-button>
-        <el-button icon="el-icon-circle-plus" circle v-if="readonlyForm = true" @click="startEdit()"></el-button>
+        <el-button icon="el-icon-circle-plus" circle v-if="readonlyForm === true" @click="startEdit()"></el-button>
     </div>
     <el-form :model="editForm" ref="editForm" :disabled="readonlyForm">
         <el-form-item>
