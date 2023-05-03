dbd15b2d4c79c4019a2392ee959d8a46433924f1
Felix <53119101+felix9611@users.noreply.github.com>
2023-05-02 21:33:03 -0400

    config changed

--- java-backend/src/main/java/com/fixedasset/mapper/AssetListMapper.java
+++ java-backend/src/main/java/com/fixedasset/mapper/AssetListMapper.java
@@ -34,51 +34,9 @@ public interface AssetListMapper extends BaseMapper<AssetList> {
             "LEFT JOIN write_off AS wo ON al.id = wo.asset_id";
     String wrapperSqlInWriteOff  = "SELECT * from ( " + querySqlInWriteOff + " ) AS q ${ew.customSqlSegment}";
 
-
-
-    String getCostYearMonth = "SELECT " +
-            "sum(cost) as totalCost," +
-            "CONCAT(YEAR(asset_list.buy_date), '-', MONTH(buy_date)) AS yearMonth " +
-            "FROM asset_list where buy_date is not null and not(cost = 0) and statu = 1 " +
-            "and sponsor = 0 and sponsor_name is null " +
-            "group by YEAR(buy_date), MONTH(buy_date) " +
-            "order by YEAR(buy_date), MONTH(buy_date)  ASC;";
-
-    String getCostYearMonthSponsor = "SELECT " +
-            "sum(asset_list.cost) as totalCost," +
-            "CONCAT(YEAR(buy_date), '-', MONTH(buy_date)) AS yearMonth " +
-            "FROM asset_list where buy_date is not null and not(cost = 0) and statu = 1 " +
-            "and sponsor = 1 and sponsor_name is null " +
-            "group by YEAR(buy_date), MONTH(buy_date) " +
-            "order by YEAR(buy_date), MONTH(buy_date)  ASC;";
-
-    String groupByType = "SELECT at.type_name as typeName , count(*) as items " +
-            "FROM asset_list as al " +
-            "left join asset_type as at on al.type_id = at.id " +
-            "where " +
-            "al.type_id is not null and al.statu = 1 " +
-            "group by type_id;";
-
-    String groupByDept = "SELECT count(*) as items, d.dept_name as deptName " +
-            "FROM asset_list " +
-            "left join department as d on asset_list.dept_id = d.id " +
-            "where asset_list.statu = 1 " +
-            "group by asset_list.dept_id;";
-
-    String groupByPlace = "SELECT count(*) as items, loc.place_name as placeName " +
-            "FROM asset_list as al " +
-            "left join location as loc on al.place_id = loc.id " +
-            "where al.statu = 1 and al.sponsor = 0 and al.sponsor_name is null " +
-            "group by al.place_id;";
     String costWithDept = "SELECT sum(al.cost) as totalCost, d.dept_name as deptName " +
             "FROM asset_list as al " +
             "left join department as d on al.dept_id = d.id " +
-            "where al.statu = 1 and al.sponsor = 0 and al.sponsor_name is null " +
-            "group by al.dept_id;";
-
-    String costWithType = "SELECT sum(al.cost) as totalCost, at.type_name as typeName " +
-            "FROM asset_list as al " +
-            "left join asset_type as at on al.type_id = at.id " +
             "where al.statu = 1 and al.sponsor = 0 and al.sponsor_name is null " +
             "group by al.dept_id;";
 
