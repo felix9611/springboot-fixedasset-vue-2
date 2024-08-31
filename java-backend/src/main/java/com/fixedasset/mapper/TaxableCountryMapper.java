7309a0cb69576471d759d3c17243e13d24af322b
Felix <53119101+felix9611@users.noreply.github.com>
2024-08-30 21:07:00 -0400

    fix

--- java-backend/src/main/java/com/fixedasset/mapper/TaxableCountryMapper.java
+++ java-backend/src/main/java/com/fixedasset/mapper/TaxableCountryMapper.java
@@ -5,9 +5,10 @@ import com.fixedasset.entity.TaxableCountry;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Select;
 
+
 import java.util.List;
 
 public interface TaxableCountryMapper extends BaseMapper<TaxableCountry>{
-    @Select("Select * from taxable_country where statumvn  = 1")
+    @Select("Select * from taxable_country where statu = 1")
     List<TaxableCountry> getALL();
 }
