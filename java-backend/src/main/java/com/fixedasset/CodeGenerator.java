8db8248e4d4119f86bd093a5b130491bf8c9a55d
Felix <53119101+felix9611@users.noreply.github.com>
2024-09-09 19:33:31 -0400

    update

--- java-backend/src/main/java/com/fixedasset/CodeGenerator.java
+++ java-backend/src/main/java/com/fixedasset/CodeGenerator.java
@@ -14,12 +14,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-// 演示例子，执行 main 方法控制台输入模块表名回车自动生成对应项目目录中
+// Demonstration example, execute the main method, enter the module table name in the console and press Enter to automatically generate the corresponding project directory.
 public class CodeGenerator {
 
     /**
      * <p>
-     * 读取控制台内容
+     * Read console content
      * </p>
      */
     public static String scanner(String tip) {
@@ -33,24 +33,24 @@ public class CodeGenerator {
                 return ipt;
             }
         }
-        throw new MybatisPlusException("请输入正确的" + tip + "！");
+        throw new MybatisPlusException("Please enter the correct" + tip + "！");
     }
 
     public static void main(String[] args) {
-        // 代码生成器
+        // code generator
         AutoGenerator mpg = new AutoGenerator();
 
-        // 全局配置
+        // Global Confi
         GlobalConfig gc = new GlobalConfig();
         String projectPath = System.getProperty("user.dir");
         gc.setOutputDir(projectPath + "/src/main/java");
         gc.setAuthor("Felix");
         gc.setOpen(false);
-        // gc.setSwagger2(true); 实体属性 Swagger2 注解
+        // gc.setSwagger2(true); Entity properties Swagger2 annotations
         gc.setServiceName("%sService");
         mpg.setGlobalConfig(gc);
 
-        // 数据源配置
+        // Data source configuration
         DataSourceConfig dsc = new DataSourceConfig();
         dsc.setUrl("jdbc:mysql://localhost:3306/vueadmin?useUnicode=true&useSSL=false&characterEncoding=utf8&serverTimezone=Asia/Shanghai");
         // dsc.setSchemaName("public");
@@ -59,13 +59,13 @@ public class CodeGenerator {
         dsc.setPassword("");
         mpg.setDataSource(dsc);
 
-        // 包配置
+        // Package Config
         PackageConfig pc = new PackageConfig();
-//        pc.setModuleName(scanner("模块名"));
+//        pc.setModuleName(scanner("Module Name"));
         pc.setParent("com.fixedasset");
         mpg.setPackageInfo(pc);
 
-        // 自定义配置
+        // Custom config
         InjectionConfig cfg = new InjectionConfig() {
             @Override
             public void initMap() {
@@ -73,18 +73,18 @@ public class CodeGenerator {
             }
         };
 
-        // 如果模板引擎是 freemarker
+        // If the template engine is freemarker
         String templatePath = "/templates/mapper.xml.ftl";
-        // 如果模板引擎是 velocity
+        // If the template engine is velocity
 //         String templatePath = "/templates/mapper.xml.vm";
 
-        // 自定义输出配置
+        // Custom output configuration
         List<FileOutConfig> focList = new ArrayList<>();
-        // 自定义配置会被优先输出
+        // Custom configurations will be output first
         focList.add(new FileOutConfig(templatePath) {
             @Override
             public String outputFile(TableInfo tableInfo) {
-                // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
+                // Customize the output file name. If your Entity sets the prefix and suffix, please note that the name of the xml will change accordingly! !
                 return projectPath + "/src/main/resources/mapper/" + pc.getModuleName()
                         + "/" + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
             }
@@ -93,10 +93,10 @@ public class CodeGenerator {
         cfg.setFileCreate(new IFileCreate() {
             @Override
             public boolean isCreate(ConfigBuilder configBuilder, FileType fileType, String filePath) {
-                // 判断自定义文件夹是否需要创建
-                checkDir("调用默认方法创建的目录，自定义目录用");
+                // Determine whether a custom folder needs to be created
+                checkDir("The directory created by calling the default method. To customize the directory, use");
                 if (fileType == FileType.MAPPER) {
-                    // 已经生成 mapper 文件判断存在，不想重新生成返回 false
+                    // The mapper file has been generated and is judged to exist. If you do not want to regenerate it, return false
                     return !new File(filePath).exists();
                 }
                 // 允许生成模板文件
@@ -107,11 +107,11 @@ public class CodeGenerator {
         cfg.setFileOutConfigList(focList);
         mpg.setCfg(cfg);
 
-        // 配置模板
+        // Template Config
         TemplateConfig templateConfig = new TemplateConfig();
 
-        // 配置自定义输出模板
-        //指定自定义模板路径，注意不要带上.ftl/.vm, 会根据使用的模板引擎自动识别
+        // Configure a custom output template
+ //Specify the custom template path, be careful not to bring .ftl/.vm, it will be automatically recognized based on the template engine used.
         // templateConfig.setEntity("templates/entity2.java");
         // templateConfig.setService();
         // templateConfig.setController();
@@ -119,20 +119,20 @@ public class CodeGenerator {
         templateConfig.setXml(null);
         mpg.setTemplate(templateConfig);
 
-        // 策略配置
+        // Strategy Config
         StrategyConfig strategy = new StrategyConfig();
         strategy.setNaming(NamingStrategy.underline_to_camel);
         strategy.setColumnNaming(NamingStrategy.underline_to_camel);
         strategy.setSuperEntityClass("BaseEntity");
         strategy.setEntityLombokModel(true);
         strategy.setRestControllerStyle(true);
-        // 公共父类
+        // public parent class
         strategy.setSuperControllerClass("BaseController");
         // 写于父类中的公共字段
         strategy.setSuperEntityColumns("id", "created", "updated", "statu");
-        strategy.setInclude(scanner("表名，多个英文逗号分割").split(","));
+        strategy.setInclude(scanner("Table name, multiple English commas separated").split(","));
         strategy.setControllerMappingHyphenStyle(true);
-//        strategy.setTablePrefix("sys_");//动态调整
+//        strategy.setTablePrefix("sys_");//Dynamic adjustment
         mpg.setStrategy(strategy);
         mpg.setTemplateEngine(new FreemarkerTemplateEngine());
         mpg.execute();
