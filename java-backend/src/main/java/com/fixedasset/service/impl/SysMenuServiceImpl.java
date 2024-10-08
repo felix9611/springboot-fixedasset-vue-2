ca4f1b2c2bfe0fb172055b91349d38c6949540d9
Felix <53119101+felix9611@users.noreply.github.com>
2024-10-08 09:18:02 -0400

    bit update

--- java-backend/src/main/java/com/fixedasset/service/impl/SysMenuServiceImpl.java
+++ java-backend/src/main/java/com/fixedasset/service/impl/SysMenuServiceImpl.java
@@ -18,7 +18,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import javax.swing.plaf.basic.BasicInternalFrameTitlePane.SystemMenuBar;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
