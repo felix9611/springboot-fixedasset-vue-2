523a47fea9c5f44f8f60d430787f3b761e48bcbd
Felix <53119101+felix9611@users.noreply.github.com>
2024-10-13 23:10:54 -0400

    JWT token update

--- java-backend/src/main/java/com/fixedasset/security/LoginSuccessHandler.java
+++ java-backend/src/main/java/com/fixedasset/security/LoginSuccessHandler.java
@@ -3,12 +3,15 @@ package com.fixedasset.security;
 import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
 import com.fixedasset.common.lang.Result;
+import com.fixedasset.entity.SysUser;
+import com.fixedasset.service.SysUserService;
 import com.fixedasset.utils.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
@@ -21,12 +24,16 @@ public class LoginSuccessHandler implements AuthenticationSuccessHandler {
 	@Autowired
 	JwtUtils jwtUtils;
 
+	@Resource private SysUserService service;
+
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
 		response.setContentType("application/json;charset=UTF-8");
 		ServletOutputStream outputStream = response.getOutputStream();
+
+		SysUser sysUser = service.getByUsername(authentication.getName());
 		
-		String jwt = jwtUtils.generateToken(authentication.getName());
+		String jwt = jwtUtils.generateToken(sysUser);
 		// response.setHeader(jwtUtils.getHeader(), jwt);
 		JSONObject loginResult = new JSONObject();
 		loginResult.putOnce("token", jwt);
